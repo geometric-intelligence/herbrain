2c4209fcd482121434765ed2024a57e296052ac0
GUIGUI Nicolas <nicolas.guigui@inria.fr>
2024-12-28 14:20:05 +0100

    smooth and extract substructures

--- herbrain/lddmm.py
+++ herbrain/lddmm.py
@@ -1,6 +1,29 @@
+import time
+import pandas as pd
 from os.path import join
 from api.deformetrica import Deformetrica
-import time
+
+
+def preprocess_full_structure(mesh, align=False, target_mesh=None):
+    new_mesh = mesh
+    if align and target_mesh is not None:
+        new_mesh = new_mesh.align(target_mesh, max_iterations=100)
+    new_mesh = new_mesh.smooth_taubin(n_iter=30)
+    target_reduction = 1 - (5000 / mesh.n_points)
+    new_mesh = new_mesh.decimate(target_reduction)
+    return new_mesh
+
+
+def preprocess_substructure(mesh, label, labels, align=False, target_mesh=None):
+    new_mesh = mesh
+    if align and target_mesh is not None:
+        new_mesh = new_mesh.align(target_mesh, max_iterations=100)
+    labels_df = pd.DataFrame(new_mesh.get_array('RGBA'))
+    labels_df['label'] = labels_df.apply(tuple, axis=1).map(labels)
+    new_mesh['label'] = labels_df['label']
+    structure = new_mesh.extract_points(
+        labels_df.loc[labels_df.label == label].index, adjacent_cells=False)
+    return structure.extract_surface().smooth_taubin(30)
 
 
 def registration(
--- herbrain/spline_regression.py
+++ herbrain/spline_regression.py
@@ -1,27 +1,47 @@
+import pandas as pd
 import pyvista as pv
 from pathlib import Path
-from herbrain.lddmm import registration, spline_regression
+from herbrain.lddmm import preprocess_full_structure, preprocess_substructure
 
 project_dir = Path('/user/nguigui/home/Documents/UCSB')
 data_dir = project_dir / 'meshes_adele' / 'b_centered'
-output_dir = project_dir / 'meshes_nicolas' / 'b_centered'
-tmp_dir = Path('/tmp')
+output_dir = project_dir / 'meshes_nico'
+output_dir.mkdir(exist_ok=True)
+smooth_dir = output_dir / 'full_structure' / 'raw'
+smooth_dir.mkdir(parents=True, exist_ok=True)
 
-# for day in range(1, 26):
-#     name = f'left_structure_-1_day{day:02}.ply'
-#     new_name = f'left_structure_t{day:02}'
-#     try:
-#         mesh = pv.read(data_dir / name)
-#         mesh.save((output_dir / new_name).with_suffix('.vtk'))
-#     except FileNotFoundError:
-#         continue
+zones = [0, 1, 5]
+for z in zones:
+    (output_dir / f'structure_{z}' / 'raw').mkdir(parents=True, exist_ok=True)
 
-day = 7
-new_name = f'left_structure_t{day:02}.vtk'
-source = output_dir / new_name
-day = 18
-new_name = f'left_structure_t{day:02}.vtk'
-target = output_dir / new_name
+ref_mesh = pv.read(data_dir / 'left_structure_-1_day01.ply')
+df = pd.DataFrame(ref_mesh.get_array('RGBA'))
+unique_rows = df.value_counts().index
+ref_labels = {row: i for i, row in enumerate(unique_rows)}
 
-registration(
-    source, target, project_dir / 'registration', kernel_width=5., metric='varifold')
+for day in range(1, 26):
+    name = f'left_structure_-1_day{day:02}.ply'
+    decimate_name = f'full_structure_t{day:02}.vtk'
+    try:
+        day_mesh = pv.read(data_dir / name)
+        preproc_mesh = preprocess_full_structure(day_mesh)
+        preproc_mesh.save(smooth_dir / decimate_name)
+
+        for z in [0, 1, 5]:
+            struc_name = f'structure_{z}_t{day:02}.vtk'
+            substruc_mesh = preprocess_substructure(day_mesh, z, ref_labels)
+            substruc_mesh.save(output_dir / f'structure_{z}' / 'raw' / struc_name)
+
+    except FileNotFoundError:
+        continue
+
+# day = 7
+# new_name = f'left_structure_t{day:02}.vtk'
+# source = output_dir.parent / 'b_centered' / new_name
+# day = 18
+# new_name = f'left_structure_t{day:02}.vtk'
+# target = output_dir.parent / 'b_centered' / new_name
+#
+# registration(
+#     source, target, project_dir / 'registration', kernel_width=5., metric='varifold',
+#     max_iter=20, print_every=1)
